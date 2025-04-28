73e7bbc1c832dc084b8ee28a106062a8b50e767c
SIR-TOPOLÁ <sirtopola@gmail.com>
2025-04-28 09:51:27 +0100

    mejoras

--- admin/registrar_estudiantes.php
+++ admin/registrar_estudiantes.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once("../componentes/header.php");
-include_once("../componentes/sidebar.php"); 
+include_once("../componentes/sidebar.php");
 
 // Consulta para obtener los países
 try {
@@ -78,72 +78,101 @@ try {
             <div class="card-body">
                 <form action="../php/guardar_estudiantes.php" method="POST" enctype="multipart/form-data">
                     <div class="row g-3">
-                        <div class="col-md-6">
+                        <div class="col-md-6 mb-2">
                             <label for="nombre_completo" class="form-label fw-bold">Nombre Completo</label>
                             <input type="text" id="nombre_completo" name="nombre_completo" class="form-control" required
                                 placeholder="Ej: María López">
                         </div>
 
-                        <div class="col-md-6">
+                        <div class="col-md-6 mb-2">
                             <label for="fecha_nacimiento" class="form-label fw-bold">Fecha de Nacimiento</label>
                             <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" class="form-control"
                                 required>
                         </div>
-
-
-                        <!-- Selección de País -->
-                        <div class="col-md-6">
-                            <div class="row">
-                                <!-- Campo de búsqueda fuera del select -->
-                                <div class="col-md-6">
-                                    <label for="buscar_pais" class="form-label fw-bold">Buscar País</label>
-                                    <input type="text" id="buscar_pais" class="form-control" placeholder="Buscar país"
-                                        onkeyup="filterCountries()">
-                                </div>
-
-                                <!-- Selección de País -->
-                                <div class="col-md-6">
-                                    <label for="pais" class="form-label fw-bold">País</label>
-                                    <select id="pais" name="pais" class="form-select" required>
-                                        <option value="" disabled selected>Selecciona tu país</option>
-                                        <?php foreach ($paises as $pais): ?>
-                                            <option value="<?= htmlspecialchars($pais['id']) ?>" class="country-item">
-                                                <?= htmlspecialchars($pais['nombre']) ?>
-                                            </option>
-                                        <?php endforeach; ?>
-                                    </select>
-                                </div>
-                            </div>
-
-
+                        <div class="col-md-6 mb-2">
+                            <label for="fecha_inicio_carrera" class="form-label fw-bold">Fecha de Inicio de
+                                Carrera</label>
+                            <input type="date" id="fecha_inicio_carrera" name="fecha_inicio_carrera"
+                                class="form-control" required>
+                        </div>
+                        <div class="col-md-6 mb-2">
+                            <label for="fecha_fin_carrera" class="form-label fw-bold">Fecha de fin de Carrera</label>
+                            <input type="date" id="fecha_fin_carrera" name="fecha_fin_carrera" class="form-control"
+                                required>
                         </div>
 
 
 
-                        <div class="col-md-6">
+                        <!-- Selección de País -->
+                        <div class="col-md-6 mb-2">
+                            <label for="pais" class="form-label fw-bold">País de estudios</label>
+                            <select id="pais" name="pais" class="form-select" required>
+                                <option value="" disabled selected>Selecciona tu país</option>
+                                <?php foreach ($paises as $pais): ?>
+                                    <option value="<?= htmlspecialchars($pais['id']) ?>" class="country-item">
+                                        <?= htmlspecialchars($pais['nombre']) ?>
+                                    </option>
+                                <?php endforeach; ?>
+                            </select>
+
+
+                        </div>
+                        <!-- Selección de ciudad -->
+                        <div class="col-md-6 mb-2">
+                            <label for="pais" class="form-label fw-bold">ciudad de estudios</label>
+                            <select id="ciudad" name="ciudad" class="form-select" required>
+                                <option value="" disabled selected>Selecciona tu ciudad</option>
+                                <?php foreach ($ciudades as $ciudad): ?>
+                                    <option value="<?= htmlspecialchars($ciudad['id']) ?>" class="country-item">
+                                        <?= htmlspecialchars($ciudad['nombre']) ?>
+                                    </option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <!-- Selección de universidad -->
+                        <div class="col-md-6 mb-2">
+                            <label for="universidad" class="form-label fw-bold">universidad</label>
+                            <select id="universidad" name="universidad" class="form-select" required>
+                                <option value="" disabled selected>Selecciona tu universidad</option>
+                                <?php foreach ($universidades as $universidad): ?>
+                                    <option value="<?= htmlspecialchars($universidad['id']) ?>" class="country-item">
+                                        <?= htmlspecialchars($universidad['nombre']) ?>
+                                    </option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+
+
+                    </div>
+
+
+                    <div class="row col-12 mt-2">
+                        <div class="col-6 ">
                             <label for="foto" class="form-label fw-bold">Foto del Estudiante</label>
                             <input type="file" id="foto" name="foto" class="form-control" accept="image/*" required
                                 onchange="previewImage(event)">
                         </div>
 
-                        <div class="col-md-12 text-center">
+                        <div class="col-6  text-center">
                             <label class="form-label fw-bold">Vista previa:</label><br>
                             <img id="foto_preview" src="#" alt="Vista previa" class="img-thumbnail rounded-4 shadow"
                                 style="max-width: 200px; display: none;">
                         </div>
-                    </div>
 
-                    <div class="d-flex justify-content-end mt-4">
-                        <button type="submit" class="btn btn-success me-2">
-                            <i class="bi bi-check-circle-fill me-1"></i> Registrar
-                        </button>
-                        <a href="estudiantes.php" class="btn btn-secondary">
-                            <i class="bi bi-x-circle-fill me-1"></i> Cancelar
-                        </a>
                     </div>
-                </form>
             </div>
+
+            <div class="d-flex justify-content-end mt-4">
+                <button type="submit" class="btn btn-success me-2">
+                    <i class="bi bi-check-circle-fill me-1"></i> Registrar
+                </button>
+                <a href="estudiantes.php" class="btn btn-secondary">
+                    <i class="bi bi-x-circle-fill me-1"></i> Cancelar
+                </a>
+            </div>
+            </form>
         </div>
+    </div>
     </div>
 </main>
 
@@ -170,28 +199,8 @@ try {
 
 
 
-    
 
-    // Función para filtrar los países en el select
-    function filterCountries() {
-        const filter = document.getElementById('buscar_pais').value.toUpperCase(); // Obtener el valor del input
-        const select = document.getElementById('pais');
-        const options = select.getElementsByClassName('country-item'); // Obtener las opciones del select
-
-        // Iterar sobre las opciones y mostrar/ocultar según el filtro
-        for (let i = 0; i < options.length; i++) {
-            const option = options[i];
-            const text = option.textContent || option.innerText;
-
-            // Mostrar la opción si el texto contiene el filtro
-            if (text.toUpperCase().indexOf(filter) > -1) {
-                option.style.display = "";
-            } else {
-                option.style.display = "none";
-            }
-        }
-    }
-
+ 
 </script>
 
 <?php include_once("../componentes/footer.php"); ?>
