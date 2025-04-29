7d8a1ae5ab7e858e323ea2966233cd6cbcf4478e
MP-PROGRAMADOR <mpprogramacion22@gmail.com>
2025-04-29 10:23:55 +0100

    hola

--- php/login.php
+++ php/login.php
@@ -6,6 +6,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = trim($_POST['email']);
     $contrasena = $_POST['contrasena'];
 
+    
+
+
     if (!empty($email) && !empty($contrasena)) {
         try {
             $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
@@ -19,17 +22,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $_SESSION['usuario_nombre'] = $usuario['nombre'];
                 $_SESSION['usuario_email'] = $usuario['email'];
 
-                // Verificar si tiene foto de perfil
-                if (!empty($usuario['foto_perfil'])) {
-                    // Si tiene foto de perfil, redirigir a la página principal
-                    header("Location: ../admin/panel_estudiante.php");
-                } else {
-                    // Si no tiene foto de perfil, redirigir a la página de perfil
-                    header("Location: ../estudiante/perfil.php");
-                }
+                header("Location: ../admin/index.php");
                 exit;
             } else {
-                $_SESSION['error'] = 'Error de Email o Contraseña incorrectos.';
+
+                
+
+                $_SESSION['error'] = 'Error de Email.';
                 header('Location: ../index.php');
                 exit;
             }
@@ -41,4 +40,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
-
